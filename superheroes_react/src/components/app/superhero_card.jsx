a8a343c103dbf5ea5c72d8563bfbc18cdb504ef0
Daniya <daniyashagieva@gmail.com>
2022-11-16 12:47:32 +0300

    added other cards

--- superheroes_react/src/components/app/superhero_card.jsx
+++ superheroes_react/src/components/app/superhero_card.jsx
@@ -4,13 +4,13 @@ function Superheroes(props) {
     return (
         <div className="superhero">
         <h2>{props.name}</h2>
-        <div><img src={props.urlPhoto}/></div>
-        <div>Вселенная: {props.universe}</div>
-        <div>Альтер эго: {props.alterEgo}</div>
-        <div>Альтер эго: ${props.occupation}</div>
-        <div>Альтер эго: ${props.friends}</div>
-        <div>Альтер эго: ${props.superpower}</div>
-        <div>Подробнее: ${props.description}</div>
+        <div><img src={props.urlPhoto} alt="superhero"/></div>
+        <div><span className="superhero_title">Вселенная:</span> {props.universe}</div>
+        <div><span className="superhero_title">Альтер эго:</span>  {props.alterEgo}</div>
+        <div><span className="superhero_title">Род деятельности:</span>  {props.occupation}</div>
+        <div><span className="superhero_title">Друзья:</span>  {props.friends}</div>
+        <div><span className="superhero_title">Суперсилы:</span>  {props.superpower}</div>
+        <div><span className="superhero_title">Подробнее:</span>  {props.description}</div>
         </div>
         );
 }
